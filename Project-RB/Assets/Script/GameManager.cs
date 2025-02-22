b5ef8365b8904a3e8d2c61bd34e0753a5c25ac4c
unknown <breydi52@gmail.com>
2025-02-22 09:08:15 -0400

    Agregacion de movimiento a la escena

--- Project-RB/Assets/Script/GameManager.cs
+++ Project-RB/Assets/Script/GameManager.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public RawImage background, platform;
+    public float parallaxSpeed = 0.02f;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float finalSpeed = parallaxSpeed * Time.deltaTime;
+        background.uvRect = new Rect(background.uvRect.x+finalSpeed, 0f, 1f, 1f);
+        platform.uvRect = new Rect(platform.uvRect.x+finalSpeed*4, 0f, 1f, 1f);
     }
 }

6fc9bccdf9ede208e3b21b11cc4259d6259a106f
Breydi19 <breydi52@gmail.com>
2025-02-23 14:02:03 -0400

    Ajustes de animaciones
    agregamos las animaciones

--- Project-RB/Assets/player2.cs
+++ Project-RB/Assets/player2.cs
@@ -3,42 +3,67 @@
 public class player2 : MonoBehaviour
 {
     public float speed = 5f;
-    public float jumpForce = 5f; // Fuerza del salto
-    public float rayLength = 0.5f; // Longitud del rayo para detectar el suelo
-    public LayerMask groundLayer; // Capa del suelo para detecci�n
+    public float jumpForce = 5f; 
+    public float rayLength = 0.5f; 
+    public LayerMask groundLayer; 
 
     private Rigidbody2D rb;
+    private Animator anim;
     private bool isGrounded;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>(); 
     }
 
     void Update()
     {
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+        float moveInput = Input.GetAxis("Horizontal"); 
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y); 
 
-        isGrounded = IsGrounded();
+        isGrounded = IsGrounded(); 
 
+        
+        if (anim != null)
+        {
+            anim.SetBool("isRunning", moveInput != 0); 
+            anim.SetBool("isJumping", !isGrounded); 
+        }
+
+        
         if (isGrounded && Input.GetButtonDown("Jump"))
-            Jump();
+        {
+            Jump(); 
+        }
+
+        
+        if (moveInput > 0) 
+        {
+            transform.localScale = new Vector3(1, 1, 1); 
+        }
+        else if (moveInput < 0) 
+        {
+            transform.localScale = new Vector3(-1, 1, 1); 
+        }
     }
 
+
+    
     void Jump()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce); 
     }
 
+    
     bool IsGrounded()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayLength, groundLayer);
         UnityEngine.Debug.DrawRay(transform.position, Vector2.down * rayLength, Color.red);
-
-        return hit.collider != null;
+        return hit.collider != null; 
     }
 
+    
     void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
